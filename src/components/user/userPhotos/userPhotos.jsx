1f7f53e6b8a7d5350257b6acb2a53abfe244491d
Marcos Rios <marcosrioos@gmail.com>
2022-07-19 14:55:20 -0300

    finished styles

--- src/components/user/userPhotos/userPhotos.jsx
+++ src/components/user/userPhotos/userPhotos.jsx
@@ -229,34 +229,50 @@ export function UserPhotos({ userInfo }) {
             <Button className='buttonUserExit' onClick={(e) => handleSignOut(e)}>Cerrar sesion</Button>
             
       <img className="photosLogo" src="../images/LogoNegro.png" alt="" />
+      
             <div className='userPhotosTitle'>
-            <strong>Bienvenido/a {userInfo?.name} a tu QBOOK.</strong>
+            <strong>Bienvenid@ {userInfo?.name} a tu QBOOK.</strong>
             </div>
             <div className='userPhotosCenter'>
-            <div className='userPhotosPie'>Descargá las fotos que más te gusten o utilizá el botón <strong>  DESCARGAR TODO.</strong></div>
-            <div className='userPhotosPie'><strong>Recordá que tu book estará disponible durante 15 días desde que iniciaste sesión.</strong></div>
+            {/* <div className='userPhotosPie'>Descargá las fotos que más te gusten o utilizá el botón <strong>  DESCARGAR TODO.</strong></div>
+            <div className='userPhotosPie'><strong>Recordá que tu book estará disponible durante 15 días desde que iniciaste sesión.</strong></div> */}
+            <div className='userPhotosPie'>
+            ¡Descarga las fotos que más te gusten! 
+            </div>
+            <div className='userPhotosPie'>Recordá que tu QBook estará disponible 15 días desde que iniciaste sesión.</div>
+            <div className='userPhotosPie'>¡Que disfrutes las fotos!</div>
             <br />
             <br />
                         {/* ---NUEVO BOTON */}
             <div className='downloadAllButton'>
-            <div className="phoneText">
-                Elegí el link que mas te convenga, si elegis PC vas a descargar un archivo .ZIP con todas las fotos de tu viaje de egresados.
+            <div className="containerPCText">
+                <div className='PCText'> 
+                Elegí el link que mas te convenga, si elegís PC vas a descargar un archivo .ZIP con todas las fotos de tu viaje de egresados.
+                </div>
             </div>
             <a className='homeLink' href={`${coordinator.dropbox}`} target='_blank' rel='noreferrer'>
             <Button className='descargarTodo'>
-            Descargar todo
+            Descargar PC
             </Button>
             </a>
             </div>
                         {/* -------  */}
             <div className='downloadAllButton'>
-            <div className="phoneText">
-                Elegí el link que mas te convenga, si elegis CELULAR nuestro boton va a dirigirte a Dropbox, no es necesario descargar la aplicación. Solo ingresas a la excursión que desees y elegis la foto que mas te guste y pones descargar!
+            <div className="containerPCText">
+            <div className='PCText'> 
+            Elegí el link que mas te convenga, si elegís CELULAR nuestro botón va a dirigirte a Dropbox, no es necesario descargar la aplicación.
+                </div>
+                <div className='PCText'>
+                Solo ingresas a la excursión que desees y elegís la foto que
+                </div>
+                <div className='PCText'>
+                más te guste y pones descargar!
+                </div>
             </div>
             {/* <UserDownloadModal downloadZip={downloadZip}/> */}
             <a className='homeLink' href={`${coordinator.celular}`} target='_blank' rel='noreferrer'>
             <Button className='descargarTodo'>
-            Descargar todo
+            Descargar CELULAR
             </Button>
             </a>
             </div>
